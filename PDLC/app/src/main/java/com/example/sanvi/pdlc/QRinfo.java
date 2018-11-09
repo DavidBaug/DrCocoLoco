49a306e3ef3947842fde87100a7bb05fd23f508a
David Gil Bautista <davidgilbautista@gmail.com>
2018-11-09 12:57:07 +0100

    Cambios diseño

--- PDLC/app/src/main/java/com/example/sanvi/pdlc/QRinfo.java
+++ PDLC/app/src/main/java/com/example/sanvi/pdlc/QRinfo.java
@@ -2,6 +2,7 @@ package com.example.sanvi.pdlc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,11 +34,13 @@ public class QRinfo extends AppCompatActivity {
         raw=extra.getString("raw");
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qr);
+        setContentView(R.layout.activity_qrinfo);
 
         nombre= findViewById(R.id.textViewNombre);
         imagen=findViewById(R.id.imageView);
         texto=findViewById(R.id.textViewSalas);
+
+        texto.setMovementMethod(new ScrollingMovementMethod());
 
         String[] parts = raw.split("###");
 
